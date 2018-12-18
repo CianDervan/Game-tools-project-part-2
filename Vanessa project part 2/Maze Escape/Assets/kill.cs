b2837439a534a438127538ac7fad24bb65bfc69e
CianDervan <44029453+CianDervan@users.noreply.github.com>
2018-12-18 16:47:18 +0000

    Base Level and enemys killable

---
+++ Vanessa project part 2/Maze Escape/Assets/kill.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kill : MonoBehaviour {
+
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+     void OnTriggerEnter (Collider other)
+    {
+      if (other.gameObject.tag == "bullet" )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
